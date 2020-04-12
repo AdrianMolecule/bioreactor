2b9b262aaa54231233cde62ec4a7a012e3aecb0f
Adrian Burlacu <63519230+adrian-burlacu-software@users.noreply.github.com>
2020-04-11 21:05:27 -0400

    Overview of the bioreactor software.

--- server/public/bioreactor-monitor/src/app/app.component.ts
+++ server/public/bioreactor-monitor/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'bioreactor-monitor';
+  title = 'Bioreactor Server Monitor';
 }
